2dfaa4ad6fa2d1e4faa7bc64328a0b08313a14e2
Christoph Gaffikin <65437946+CmonDudeMan@users.noreply.github.com>
2021-10-15 22:59:34 -0400

    Add files via upload

---
+++ testing.java
@@ -0,0 +1,13 @@
+package lab5;
+
+public class testing {
+
+	public static void main(String[] args) {
+
+		String userText = "hello";
+		String x = userText.replace("hello","guvna ");
+		System.out.println(x);
+
+	}
+
+}
